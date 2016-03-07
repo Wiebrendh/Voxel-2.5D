099a54c57711e9ba9c1e398b45c69daed61eb326
user <user@dsd115a-d004184.local>
2016-03-07 15:57:29 +0100

    update

---
+++ Assets/Scripts/Player/Inventory/Inventory.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public enum ItemType { Tool, Block };
+
+public class Inventory : MonoBehaviour 
+{
+
+	public List<Item> items = new List<Item>();
+
+	void Start () 
+	{
+
+	}
+
+	void Update () 
+	{
+		
+	}
+}
+
+[System.Serializable]
+public class Item 
+{
+	public string name;
+	public int id;
+	public ItemType type;
+
+	// Tool data
+	public float dps;
+
+	// Block data
+	public int damage;
+
+	public Item (string name, int id, ItemType type) // Tool
+	{
+		this.name = name;
+		this.id = id;
+		this.type = type;
+	}
+}
+
+[System.Serializable]
+public class Tool
+{
+
+	public Item item;
+
+	public Tool (Item item)
+	{
+		this.item = item;
+	}
+}
---
+++ Assets/Scripts/Player/Inventory/Items.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Items : MonoBehaviour 
+{
+
+	public Inventory inventory;
+
+	void Start () 
+	{
+		// Tools
+
+		// Blocks
+	}
+}
